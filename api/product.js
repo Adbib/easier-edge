a456c2566d54dcbc1a03e90e55671345470bf15e
Adbib <mradbib@gmail.com>
2022-07-23 15:21:56 +0100

    bla

--- api/product.js
+++ api/product.js
@@ -158,9 +158,7 @@ router.get("/", async (req, res) => {
 router.post("/platform", encodeUrl, (req, res) => {
   // console.log(req.body.shop);
   if (ACTIVE_SHOPIFY_SHOPS[req.body.shop] === undefined) {
-    res.redirect(
-      `/api/shopify/${$req.body.platform}/auth`
-    );
+    res.redirect(`/api/shopify/${req.body.platform}/auth`);
   } else {
     res.send("Hello world!");
   }
